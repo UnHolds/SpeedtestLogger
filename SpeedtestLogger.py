cfde0caaf67643327dcc8567908c5bc27b41f8bd
Bene <benedikt.metzler@gmail.com>
2022-07-30 17:04:38 +0200

    Added protection against doubletesting Min. timedifference between test can be set to x minutes

--- SpeedtestLogger.py
+++ SpeedtestLogger.py
@@ -22,18 +22,25 @@ def index():
 measurements = []
 
 def main():
+    lastTest = datetime.datetime.now()
     while(True):
         time.sleep(30)
         if(int (datetime.datetime.now().strftime("%M")) % 5 == 0):
-            now = datetime.datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
-            st = speedtest.Speedtest()
-            st.get_best_server()
+            now = datetime.datetime.now()
+            if (now - lastTest > datetime.timedelta(minutes=1)):
+                lastTest = now
+                st = speedtest.Speedtest()
+                st.get_best_server()
+                
+                ping = st.results.ping
+                download = round(st.download() / 1000 / 1000, 1)
+                upload = round(st.upload() / 1000 / 1000, 1)
+                measurements.append((now.strftime("%Y/%m/%d - %H:%M"),ping,download,upload))
+                #print(measurements)
+                
             
-            ping = st.results.ping
-            download = round(st.download() / 1000 / 1000, 1)
-            upload = round(st.upload() / 1000 / 1000, 1)
-            measurements.append((now,ping,download,upload))
-            print(measurements)
+        
+
 
 
 
